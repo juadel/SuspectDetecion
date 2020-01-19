68542cfd2e277d91de515db697ac0f33b3432ccb
Juan Delgado <juadel@hotmail.com>
2020-01-18 23:03:09 -0700

    first commit

---
+++ Client/request_sms.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+API_endpoint = "https://46p7nhmwib.execute-api.ca-central-1.amazonaws.com/dev/SuspectNotification"
+
+def request_sms(suspect):
+
+    body = {'phone_number':'+17807103155','suspect':suspect}
+    headers={}
+    r = requests.post(url=API_endpoint, data=json.dumps(body), headers=headers)
+    print("SMS send:%s"%r.text)
+
+request_sms("Juan")
